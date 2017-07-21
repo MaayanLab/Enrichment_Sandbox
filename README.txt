7a97887516a5c26a7429be6b45c886f077d02a4a
damondpham <damondpham@gmail.com>
2017-07-21 12:36:53 -0400

    more details in readme

--- README.txt
+++ README.txt
@@ -1,10 +1,37 @@
 ===A brief guide:===
 
 How to use:
-*Run setup.py .
-*get_rankings.py has a function called get_methods(). In here, create a pandas.DataFrame with methods and parameters from enrichment_methods.py which you want to use.
-*Run get_rankings.py .
-*Run evaluate_rankings.py to plot the results.
+1. Run setup.py .
+2. get_rankings.py has a function called get_methods(). In here, create a pandas.DataFrame with function and parameters from enrichment_methods.py which you want to use. Then, run get_rankings.py .
+3. Run evaluate_rankings.py to plot the results.
+
+
+===More details:===
+
+What happens behind the scenes, in pseudocode:
+
+1. setup.py
+*The gmt files are transformed into gene-vector format, and other necessary files are downloaded and processed.
+
+2. get_scores.py
+*For each ('label, 'feature') pair of gmt libraries:
+	*Get a list of experiments in the 'label' library whose corresponding transcription factor is also found in a 'feature' library experiment
+	*(From here on, this list will be referred to as the 'label' library transcription factors)
+	*For each enrichment method:
+		*For each transcription factor in the aforementioned 'label' library list:
+			*Perform enrichment by inputting this transcription factor's gene set into the whole 'feature' library
+			*The output is a list of enrichment scores for each experiment in the 'feature' library
+		*The list of list of enrichment scores is stored as a matrix in a .csv file
+		*(Columns: label library tfs. Indices: feature library experiments. Cell values: scores given by the enrichment method.)
+
+3. evaluate_scores.py
+*For each ('label, 'feature') pair of gmt libraries:
+	*The corresponding .csv files from get_scores.py are loaded
+	*For each corresponding .csv file (in other words, for each method):
+		*For each column (in other words, for each label library tf):
+			*The ranks for experiments which correspond to the same tf used to perform enrichment are aggregated.
+*Methods are grouped as desired (for example, by library or function), then plotted
+
 
 Naming conventions:
 *f: file
@@ -18,14 +45,17 @@ Naming conventions:
 *params: parameters for the function
 *method: a specific function & paramaters combination which is being evaluated
 
+
 What is meant by 'label' and 'feature' library?:
 The 'label library' is the library whose tf gene sets are being inputted for enrichment analysis.
 The 'feature library' is the library which is being used as the background for enrichment analysis.
 In other words, we take each tf from the label library, and perform enrichment with it using all of the feature library.
 (Think of ML notation: genes are the samples, and membership within each 'feature library' gene set are the features. We want to build a classifier which can label whether or not the gene belongs to the 'label library' tf gene set. From this classifier, we extract the feature importances to get the list of enriched 'feature library' tfs.)
 
-How are the results stored and evaluated?:
-Results are stored as csv files for each specified method & param combination, for each library pair.
-The columns of these files are the label library tfs. The indices are the feature library tfs. The cell values are the scores given by the enrichment method.
+
+Justification for the benchmarking methodology:
 Ideally, the experiments with the best scores will correspond to the same the tf which was used to perform enrichment - this is how we benchmark the different enrichment methods!
-[Talk about the bridge plots]
+
+
+Interpretation of the bridge plots:
+[In progress]
---
+++ get_scores.py
@@ -0,0 +1,128 @@
+import csv
+import os
+import numpy as np
+import pandas as pd
+from joblib import Parallel, delayed
+import enrichment_methods as m
+from setup import convert_gmt
+import h5py
+
+def clean(tf):
+	'''Extracts the transcription factor name from the name of a gmt experiment.'''
+	return str(tf).partition('_')[0].partition(' ')[0].upper()
+
+def get_overlaps(label_tfs, feature_tfs):
+	'''Returns a list of label library experiments whose transcription factors are also in any feature library experiment.
+	label_tfs: list-like object
+	feature_tfs: list-like object
+	'''
+	cleaned_overlaps = {clean(x) for x in feature_tfs} & {clean(x) for x in label_tfs}
+	l_in_overlaps = [x for x in label_tfs if clean(x) in cleaned_overlaps]
+	return l_in_overlaps
+
+def get_methods_and_params(l,f, l_name, f_name):
+	'''
+	######################################################################################################################################
+	This is where you choose which enrichment methods to run, and which paramaters to use!
+	This function will return a dataframe with index ['method', 'params'], and columns as the names of each enrichment method/param combo.
+	For example, a column with name 'Foo3' might have 'method' m.Foo() and 'params' bootstrap = False.
+	See enrichment_methods.py for the available methods and necessary params.
+	You must specify ALL the params EXCEPT for l_tf_genes, which is initialized and called later, in enrichment_wrapper().
+	######################################################################################################################################
+	l : pandas.DataFrame
+		the "label" gmt library, from which tfs are being used as input gene sets
+	f: pandas.DataFrame
+		the "feature" gmt library, whose tfs are being ranked by the enrichment method
+	l_name : str
+		the name of l, for example "ChEA_2016"
+	f_name: str
+		the name of f, for example "CREEDS"
+	'''
+	#Get the ARCHS4 correlation data.
+	os.chdir('..')
+	os.chdir('libs')
+	ARCHS4 = h5py.File(l_name + '_ARCHS4_corr.h5', 'r+')
+	os.chdir('..')
+	os.chdir('results')
+	h_genes = ARCHS4['human']['meta']['genes']
+	m_genes = ARCHS4['mouse']['meta']['genes']
+	ARCHS4_genes_dict = {'human': pd.Series(np.arange(len(h_genes)), index=h_genes[...]),
+		'mouse': pd.Series(np.arange(len(m_genes)), index=m_genes[...])}
+	ARCHS4.close()
+
+	#(Define any other variables, as necessary, here.)
+
+	#Create the output dataframe.
+	df = pd.DataFrame(index=['func', 'params'])
+	df['Control'] = [m.Control, ([f.columns.values])]
+	df['Fisher'] = [m.Fisher, ([f])]
+	#df['FAV'] = [m.FisherAdjusted, (f, l_name, f_name, ARCHS4_genes_dict)]
+	df['RandomForest'] = [m.Forest, (f, f.columns.values, 73017, None, True, None, None)]
+
+	return df
+
+def enrichment_wrapper(pair):
+	'''This function is called for each lib pair, and iterates over each method and each tf. 
+	pair : dict
+		key 'l' contains the label library df, and key 'f' contains the feature library df. 
+	'''
+	l_name, f_name = pair['l'].index.name, pair['f'].index.name
+	print('Beginning enrichment analysis from', l_name, 'to', f_name)
+
+	#Get the label library experiments whose transcription factors are also in any feature library experiment.
+	overlaps = get_overlaps(pair['l'].columns.values, pair['f'].columns.values)
+
+	#Get the methods and parameters with which to perform enrichment. 
+	methods_and_params = get_methods_and_params(pair['l'], pair['f'], l_name, f_name)
+	output_heading = 'from_' + l_name + '_to_' + f_name
+
+	#Iterate over each method.
+	for column in methods_and_params:
+		mp = methods_and_params[column]
+
+		#Some methods actually return multiple results, and so will need multiple output files.
+		if mp.name == 'ZAndCombined': 
+			output_fnames = (output_heading + '_Z.csv', output_heading + '_Combined.csv')
+		elif mp.name == 'FAV':
+			output_fnames = [output_heading + '_FisherAdjusted' + str(x) + '.csv' for x in range(2,11)]
+		else: 
+			output_fnames = (output_heading + '_' + mp.name + '.csv',)
+
+		#Check if the file has already been created.
+		if os.path.isfile(output_fnames[0]): print('ranking file already created for', mp.name)
+
+		#If not, create it. 
+		else:
+			#Use dataframes to store results after each tf iteration.
+			dfs = [pd.DataFrame()] * len(output_fnames)
+			#Iterate over each tf in the overlaps.
+			for l_tf in overlaps:
+				print(mp.name, l_tf) #for diagnostics
+				l_tf_genes = pair['l'].index[pair['l'][l_tf]]
+				result = mp['func'](l_tf_genes, *mp['params'])
+				for x in range(len(dfs)): 
+					df = dfs[x]
+					#Store result as a column in the df.
+					if len(dfs) == 1: df[l_tf] = result
+					else: df[l_tf] = result[x]
+			for x in range(len(dfs)): 
+				#Save the dfs.
+				df = dfs[x]
+				df.index = pair['f'].columns
+				df.to_csv(output_fnames[x], sep='\t')
+	return
+
+if __name__ == '__main__':
+	all_libs = ['CREEDS', 'ENCODE_TF_ChIP-seq_2015', 'ChEA_2016']
+
+	#Get dataframes of each gmt library in all_libs
+	os.chdir('libs')
+	all_dfs = {x:convert_gmt('df', x) for x in all_libs}
+	os.chdir('..')
+	if not os.path.isdir('results'): os.makedirs('results')
+	os.chdir('results')
+
+	#Iterate over each gmt pair.
+	lib_df_pairs = [{'l':all_dfs[a], 'f':all_dfs[b]} for a in all_dfs for b in all_dfs if a.partition('_')[0] != b.partition('_')[0]]
+
+	Parallel(n_jobs=6, verbose=0)(delayed(enrichment_wrapper)(pair)for pair in lib_df_pairs)
